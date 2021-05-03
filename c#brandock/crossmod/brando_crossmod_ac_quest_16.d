16b4f6079028479bf4d9f12f409432f841bf6583
Gitjas <Gitjas@users.noreply.github.com>
2021-05-03 05:49:20 +0200

    Thalantyr should say his line

--- c#brandock/crossmod/brando_crossmod_ac_quest_16.d
+++ c#brandock/crossmod/brando_crossmod_ac_quest_16.d
@@ -3,6 +3,6 @@
 */
 I_C_T ~THALAN~ %thalan_80% C#Brandock_thalan_ac80
 == ~c#brandj~ IF ~OR(2) InParty("C#Brandock") Global("C#BrandockJoined","GLOBAL",2) InMyArea("C#Brandock") !StateCheck("C#Brandock",CD_STATE_NOTVALID)~ THEN @10 /* ~[Brandock]Was? Nein, das kann nicht Euer Ernst sein! Das ist doch ein Mensch... hinter dem Wolf, und Ihr wisst, wie man ihm hilft. Das meintet Ihr doch bestimmt nicht ernst?~ */
-== ~c#brandj~ IF ~OR(2) InParty("C#Brandock") Global("C#BrandockJoined","GLOBAL",2) InMyArea("C#Brandock") !StateCheck("C#Brandock",CD_STATE_NOTVALID)~ THEN @11 /* ~[Thalantyr]Junger Magier, die Erfahrung hat mich gelehrt, dass man nicht immer allen helfen kann - und auch wenn man es könnte, es in manchen Fällen mehr Leid verursacht, als das Missgeschick eines Einzelnen als Anlass zu nehmen, eine grundsätzliche Lösung für ein Unheil zu finden, das sicher noch weitere Leben in Gefahr bringen wird.~ */
+== ~THALAN~ IF ~OR(2) InParty("C#Brandock") Global("C#BrandockJoined","GLOBAL",2) InMyArea("C#Brandock") !StateCheck("C#Brandock",CD_STATE_NOTVALID)~ THEN @11 /* ~[Thalantyr]Junger Magier, die Erfahrung hat mich gelehrt, dass man nicht immer allen helfen kann - und auch wenn man es könnte, es in manchen Fällen mehr Leid verursacht, als das Missgeschick eines Einzelnen als Anlass zu nehmen, eine grundsätzliche Lösung für ein Unheil zu finden, das sicher noch weitere Leben in Gefahr bringen wird.~ */
 END
 
