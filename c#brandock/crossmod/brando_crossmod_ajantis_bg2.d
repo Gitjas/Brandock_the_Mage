b0f5f1225460f6b3f695cec927f4e17382fe70c4
Gitjas <jastey@web.de>
2024-11-02 21:02:57 +0100

    -corrected stutter bug for crossmod Ajantis BGII

--- c#brandock/crossmod/brando_crossmod_ajantis_bg2.d
+++ c#brandock/crossmod/brando_crossmod_ajantis_bg2.d
@@ -2,7 +2,7 @@
 APPEND c#brandj
 
 IF WEIGHT #-1
-~Global("C#Br_LorePathARAC04BG2","GLOBAL",1)~ THEN lorepath_aj04
+~Global("C#Br_LorePathC#AJ04BG2","GLOBAL",1)~ THEN lorepath_aj04
 SAY @0 /* ~Oh, books - collected by a dragon. <CHARNAME>... do we have the time? Please, this is exciting just by looking at the covers!~ */
 ++ @1 /* ~How could I deny that. Take as much wisdom from this place as you can.~ */ + lorepath_aj04_02
 ++ @2 /* ~I'm sorry, Brandock. I don't feel comfortable enough to linger here.~ */ + lorepath_aj04_01
@@ -10,7 +10,7 @@ END
 
 IF ~~ THEN lorepath_aj04_01
 SAY @4
-IF ~~ THEN DO ~SetGlobal("C#Br_LorePathARAC04BG2","GLOBAL",2)~ EXIT
+IF ~~ THEN DO ~SetGlobal("C#Br_LorePathC#AJ04BG2","GLOBAL",2)~ EXIT
 END
 
 END //APPEND
@@ -39,4 +39,4 @@ See("Keldorn")
 == c#brandj @6
 END
 IF ~~ THEN DO ~ReallyForceSpellRES("c#brlob2",Myself) IncrementGlobal("C#Br_LorePathIncrease","GLOBAL",1)
-SetGlobal("C#Br_LorePathARAC04BG2","GLOBAL",2) ClearAllActions() StartCutSceneMode() StartCutScene("c#brftb")~ EXIT
+SetGlobal("C#Br_LorePathC#AJ04BG2","GLOBAL",2) ClearAllActions() StartCutSceneMode() StartCutScene("c#brftb")~ EXIT
