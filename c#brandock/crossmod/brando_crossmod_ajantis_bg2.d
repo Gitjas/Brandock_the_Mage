ebb004421b2174b3f69dac02ac49083f9b4b9080
Gitjas <Gitjas@users.noreply.github.com>
2026-01-09 08:20:37 +0100

    Fixed install error for crossmod with Ajantis BG2.

--- c#brandock/crossmod/brando_crossmod_ajantis_bg2.d
+++ c#brandock/crossmod/brando_crossmod_ajantis_bg2.d
@@ -46,12 +46,12 @@ I_C_T c#aja25j %ajantis_saradush_34% C#BranToB_c#aja25j_34
 Detect("C#Brandock") !StateCheck("C#Brandock",CD_STATE_NOTVALID)
 OR(3)
 !InParty("sarevok") !InMyArea("sarevok") StateCheck("sarevok",CD_STATE_NOTVALID)~ THEN ~[Brandock]I haven't felt that bad since Thurdon made himself explode during the magic apprenticeship with Theos. That moment where you know how to help but then realize it's too late... Er, I am babbling again. I am sorry.~
-EXIT
+END
 
 I_C_T SAREV25J %ajantis_saradush_35% C#BranToB_c#aja25j_35
 == C#brandJ IF ~OR(2) InParty("C#Brandock") Global("C#BrandockJoined","GLOBAL",2)
 Detect("C#Brandock") !StateCheck("C#Brandock",CD_STATE_NOTVALID)~ THEN ~[Brandock]I haven't felt that bad since Thurdon made himself explode during the magic apprenticeship with Theos. That moment where you know how to help but then realize it's too late... Er, I am babbling again. I am sorry.~
-EXIT
+END
 
 /* ~[Brandock]I mean, duh. We are inside a besieged city.~ */
 I_C_T C#BrandJ %in_saradush% C#AjToB_brandj_insaradush
